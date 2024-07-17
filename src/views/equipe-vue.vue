958e40d77b47696fe027eb1bdae7ca262761dd0c
kayquelopes <kayquelopes353@gmail.com>
2024-07-17 11:08:26 -0300

    first commit

---
+++ src/views/equipe-vue.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+    name: 'EquipeView',
+    data() {
+        return {
+            equipe: [
+                {
+                    nome: 'Kayque',
+                },
+                {
+                    nome: 'Murilo',
+                },
+                {
+                    nome: 'jeffrey',
+                },
+                {
+                    nome: 'Rafael',
+                },
+                {
+                    nome: 'Daniel',
+                },
+                {
+                    nome: 'Arthur',
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h1>Equipe</h1>
+        <ul>
+            <li v-for="membro in equipe" :key="membro.nome">
+                <h2>{{ membro.nome }}</h2>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+/* Your component-specific styles go here */
+</style>
